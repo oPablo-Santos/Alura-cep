0e79777a2dc9db003dfe18a1b76946cd46e4891d
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-03-01 12:48:03 -0300

    update

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 async function buscaEndereco(cep) {
+  var mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
   try {
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     var consultaCEP2 = await consultaCEP.json();
     if (consultaCEP2.erro) {
       throw Error("CEP não existe!");
     }
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");
+    var bairro = document.getElementById("bairro");
+
+    cidade.value = consultaCEP2.localidade;
+    logradouro.value = consultaCEP2.logradouro;
+    estado.value = consultaCEP2.uf;
+    bairro.value = consultaCEP2.bairro;
+
     console.log(consultaCEP2);
     return consultaCEP2;
   } catch (erro) {
+    mensagemErro.innerHTML = `<p> Cep não encontrado - Tente novamente </p>`;
     console.log(erro);
   }
 }
 
-let ceps = ["02993310", "01001000"];
-let conjuntoCeps = ceps.map((valores) => buscaEndereco(valores));
-console.log(conjuntoCeps);
-Promise.all(conjuntoCeps).then((respostas) => console.log(respostas));
-
-
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
